5dcc3082cdd1e69f07fd5fd33286ee29500c089a
Oriol Ustrell <oriol.ustrell@mango.com>
2022-11-24 14:35:15 +0100

    from scratch

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
-import { createRoot } from "react-dom/client";
-import "utils/service-worker-registration";
+import { createRoot } from 'react-dom/client';
+// import "utils/service-worker-registration";
 
-const root = createRoot(document.getElementById("app"));
+const root = createRoot(document.getElementById('app'));
 
 root.render(<h1>Hello, world!</h1>);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-const path = require("path");
-const fs = require("fs");
+const path = require('path');
+const fs = require('fs');
 
-const SW_LOCATION = "./src/service-workers/";
+const SW_LOCATION = './src/service-workers/';
 
 const serviceWorkersEntrys = () => {
   let swFiles = fs.readdirSync(SW_LOCATION);
@@ -9,57 +9,57 @@ const serviceWorkersEntrys = () => {
   return swFiles.reduce(
     (entries, file) => ({
       ...entries,
-      [file.split(".")[0]]: `${SW_LOCATION}${file}`,
+      [file.split('.')[0]]: `${SW_LOCATION}${file}`,
     }),
     {}
   );
 };
 
 module.exports = (env, argv) => {
-  const isDevelopment = (argv && argv.mode) === "development";
+  const isDevelopment = (argv && argv.mode) === 'development';
   return [
     {
       entry: { main: `./src/index.jsx`, ...serviceWorkersEntrys() },
       output: {
-        path: path.join(__dirname, "public"),
-        publicPath: "auto",
-        filename: "[name].js",
+        path: path.join(__dirname, 'public'),
+        publicPath: 'auto',
+        filename: '[name].js',
       },
       devServer: {
-        static: "./public",
-        allowedHosts: "all",
+        static: './public',
+        allowedHosts: 'all',
         historyApiFallback: true,
-        port: 3030,
+        port: 3031,
         client: {
           overlay: false,
-          logging: "none",
+          logging: 'none',
         },
       },
-      devtool: isDevelopment ? "source-map" : false,
+      devtool: isDevelopment ? 'source-map' : false,
       resolve: {
         alias: {
-          components: path.resolve(__dirname, "src/components/"),
-          hooks: path.resolve(__dirname, "src/hooks/"),
-          utils: path.resolve(__dirname, "src/utils/"),
+          components: path.resolve(__dirname, 'src/components/'),
+          hooks: path.resolve(__dirname, 'src/hooks/'),
+          utils: path.resolve(__dirname, 'src/utils/'),
         },
-        extensions: ["", ".js", ".jsx"],
+        extensions: ['', '.js', '.jsx'],
       },
       module: {
         rules: [
           {
             test: /\.(js|jsx)$/,
-            loader: "babel-loader",
+            loader: 'babel-loader',
             exclude: /node_modules/,
           },
           {
             test: /\.(svg|png|jpg|gif)$/i,
-            type: "asset/resource",
+            type: 'asset/resource',
           },
           {
             test: /\.html$/,
             use: [
               {
-                loader: "html-loader",
+                loader: 'html-loader',
                 options: {
                   minimize: true,
                 },
@@ -68,7 +68,7 @@ module.exports = (env, argv) => {
           },
           {
             test: /\.css$/,
-            use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+            use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
           },
         ],
       },
